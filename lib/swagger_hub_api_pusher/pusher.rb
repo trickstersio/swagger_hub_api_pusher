cffb66029329e0f8117c76763582deb612e6a0ac
Markov Alexey <piton4eg@mail.ru>
2017-06-29 00:07:22 +0400

    Add pushing swagger_file to Swagger Hub API

--- lib/swagger_hub_api_pusher/pusher.rb
+++ lib/swagger_hub_api_pusher/pusher.rb
@@ -1,7 +1,37 @@
+require 'httparty'
+require 'pry'
+
 module SwaggerHubApiPusher
   class Pusher
     def execute
-      puts "Pushing..."
+      raise ArgumentError, config.errors_messages unless config.valid?
+
+      response = Client.post(url,
+        body: File.read(config.swagger_file),
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => config.api_key
+        }
+      )
+
+      if response.success?
+        puts 'swagger.json was successfully posted'
+      else
+        puts JSON.parse(response.body)['message']
+      end
+    end
+
+    private def url
+      "/#{config.owner}/#{config.api_name}?version=#{config.version}"
+    end
+
+    private def config
+      @config ||= SwaggerHubApiPusher.config
+    end
+
+    class Client
+      include HTTParty
+      base_uri 'https://api.swaggerhub.com/apis'
     end
   end
 end
--- spec/swagger_hub_api_pusher/pusher_spec.rb
+++ spec/swagger_hub_api_pusher/pusher_spec.rb
@@ -5,8 +5,67 @@ describe SwaggerHubApiPusher::Pusher do
   describe '#execute' do
     subject { described_class.new }
 
-    it 'logs to stdout' do
-      expect { subject.execute }.to output("Pushing...\n").to_stdout
+    context 'when config is invalid?' do
+      before do
+        allow(SwaggerHubApiPusher.config).to receive(:valid?).and_return(false)
+      end
+
+      it 'raises error' do
+        expect { subject.execute }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when config is valid?' do
+      before do
+        SwaggerHubApiPusher.configure do |config|
+          config.owner = 'owner'
+          config.api_name = 'api_name'
+          config.api_key = 'api_key'
+          config.version = 'version'
+          config.swagger_file = 'spec/fixtures/swagger.json'
+        end
+      end
+
+      let(:url) { '/owner/api_name?version=version' }
+      let(:options) do
+        {
+          body: File.read('spec/fixtures/swagger.json'),
+          headers: {
+            'Content-Type' => 'application/json',
+            'Authorization' => 'api_key'
+          }
+        }
+      end
+      let(:successful_response) { double(success?: true) }
+      let(:failure_response) do
+        double(success?: false, body: { 'message' => 'error' }.to_json)
+      end
+
+      it 'posts swagger_file' do
+        expect(described_class::Client).to receive(:post)
+          .with(url, options).and_return(successful_response)
+        subject.execute
+      end
+
+      context 'when response succeed' do
+        before do
+          allow(described_class::Client).to receive(:post).and_return(successful_response)
+        end
+
+        it 'logs success message' do
+          expect { subject.execute }.to output("swagger.json was successfully posted\n").to_stdout
+        end
+      end
+
+      context 'when response failed' do
+        before do
+          allow(described_class::Client).to receive(:post).and_return(failure_response)
+        end
+
+        it 'logs error message' do
+          expect { subject.execute }.to output("error\n").to_stdout
+        end
+      end
     end
   end
 end
