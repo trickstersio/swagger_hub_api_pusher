cb3623975d6a85db4c0be581372932151f53691f
Markov Alexey <piton4eg@mail.ru>
2017-07-05 00:00:18 +0400

    Abort rake task if Pusher returned error message

--- spec/tasks/swagger_spec.rb
+++ spec/tasks/swagger_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
 describe 'rake swagger:push', type: :task do
-  it 'delegates execution to SwaggerHubApiPusher::Pusher' do
-    expect(SwaggerHubApiPusher::Pusher).to receive(:new).and_return(double(execute: true))
-    task.execute
+  context 'when SwaggerHubApiPusher::Pusher returns nil' do
+    before do
+      expect(SwaggerHubApiPusher::Pusher).to receive(:new)
+        .and_return(double(execute: nil))
+    end
+
+    it 'executes successful' do
+      expect { task.execute }.not_to raise_error
+    end
+  end
+
+  context 'when SwaggerHubApiPusher::Pusher returns error message' do
+    before do
+      expect(SwaggerHubApiPusher::Pusher).to receive(:new)
+        .and_return(double(execute: 'Something went wrong'))
+    end
+
+    it 'abort execution' do
+      expect { task.execute }.to raise_error SystemExit
+    end
   end
 end
